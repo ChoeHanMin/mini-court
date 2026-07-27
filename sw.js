967b711848d74e649979fec84c56caa16c89305e
Choi Hanmin <76240247+ChoeHanMin@users.noreply.github.com>
2026-07-27 22:54:06 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// 미니법정 서비스워커 - 오프라인에서도 게임이 열리도록 핵심 파일을 캐싱합니다.
+const CACHE_NAME = 'mini-court-cache-v3-6';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
